ee1a2c8036ff726f7d169630e47a77bd7a782ea5
Harsh Chilhate <abitharshchilhate@gmail.com>
2026-05-02 12:41:16 +0530

    feat: add main.py CLI input handling

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 import os
 from dotenv import load_dotenv
+import argparse
 import yaml
 import logging
 
-load_dotenv()
-
+# logging 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger(__name__)
 
+
+#get INPUT_PATH from .env
+load_dotenv()
+base_path = os.getenv("INPUT_PATH")
+if base_path is None:
+    logging.error("INPUT_PATH not set in .env")
+    exit(1)
+
+# load config
 try:
     with open("config/config.yaml", "r") as file:
         config = yaml.safe_load(file)
@@ -20,3 +29,15 @@ except FileNotFoundError:
 except yaml.YAMLError as e:
     logger.error(f"Error reading config.yaml : {e}")
     exit(1)
+
+#get image name from CLI
+ap = argparse.ArgumentParser()
+ap.add_argument("--image", required=True, help="filename of input image e.g. document.jpg")
+args = ap.parse_args()
+
+file_name = args.image
+
+
+#counstruct full path
+full_path = os.path.join(base_path, file_name)
+logger.info(f"Input path: {full_path}")
